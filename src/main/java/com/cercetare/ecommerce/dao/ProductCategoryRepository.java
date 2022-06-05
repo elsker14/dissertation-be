d8cf0549152e2704167c7b917143ad821937baa8
elsker14 <iancujianu98@gmail.com>
2022-06-05 19:58:11 +0300

    Section 10 done - everything working

--- src/main/java/com/cercetare/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/cercetare/ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.cercetare.ecommerce.dao;
 import com.cercetare.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
@@ -14,4 +16,6 @@ Info:
 
     collectionResourceRel = name of JSON entry
     path = endpoint path la URL : www.blabla.com/product-category
+
+    Adaugam adnotarea cu CrossOrigin in step60 pentru a putea permite lui SpringBoot sa accepte scripturi web cu de la surse cu origine diferita
  */
--- src/main/java/com/cercetare/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/cercetare/ecommerce/dao/ProductRepository.java
@@ -2,7 +2,14 @@ package com.cercetare.ecommerce.dao;
 
 import com.cercetare.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
+
+/*
+Info:
+    Adaugam adnotarea cu CrossOrigin in step60 pentru a putea permite lui SpringBoot sa accepte scripturi web cu de la surse cu origine diferita
+ */
