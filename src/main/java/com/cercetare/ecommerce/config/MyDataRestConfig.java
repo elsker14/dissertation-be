b8e8a50c29a669936e91c95cd7d538e9cb114ccb
Iancu Jianu <iancujianu98@gmail.com>
2022-12-20 20:55:54 +0200

    section 20: part 162 -> 167: populate countries and states Steps: 1. Create database tables 2. Develop JPA Entities (Country, State) 3. Create Spring Data Repositories 4. Update Spring Data REST Configs

--- src/main/java/com/cercetare/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/cercetare/ecommerce/config/MyDataRestConfig.java
@@ -1,7 +1,9 @@
 package com.cercetare.ecommerce.config;
 
+import com.cercetare.ecommerce.entity.Country;
 import com.cercetare.ecommerce.entity.Product;
 import com.cercetare.ecommerce.entity.ProductCategory;
+import com.cercetare.ecommerce.entity.State;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -24,6 +26,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     public MyDataRestConfig(EntityManager theEntityManager) {
         entityManager = theEntityManager;
     }
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         // Linia de mai jos era odata cu implementarea metodei.
@@ -31,21 +34,23 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
 
-        // disable HTTP methods for Product: PUT, POST and DELETE
-        config.getExposureConfiguration()
-                .forDomainType(Product.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
-
-        // disable HTTP methods for ProductCategory: PUT, POST and DELETE
-        config.getExposureConfiguration()
-                .forDomainType(ProductCategory.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+        // disable HTTP methods for Product, ProductCategory, Country, State: PUT, POST and DELETE
+        disableHttpMethods(Product.class, config, theUnsupportedActions);
+        disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
+        disableHttpMethods(Country.class, config, theUnsupportedActions);
+        disableHttpMethods(State.class, config, theUnsupportedActions);
 
         // call an internal helper method
         exposeIds(config);
     }
+
+    private static void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
+
     private void exposeIds(RepositoryRestConfiguration config) {
 
         // expose entity ids
---
+++ src/main/java/com/cercetare/ecommerce/dao/StateRepositories.java
@@ -0,0 +1,16 @@
+package com.cercetare.ecommerce.dao;
+
+import com.cercetare.ecommerce.entity.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "countries", path = "states")
+public interface StateRepositories extends JpaRepository<State, Integer> {
+
+    List<State> findByCountryCode(@Param("code") String code);
+}
