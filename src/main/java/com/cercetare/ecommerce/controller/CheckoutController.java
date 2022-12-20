24e36434695426dd4b2b8f8ad0c5bddab656c61e
Iancu Jianu <iancujianu98@gmail.com>
2022-12-21 00:39:46 +0200

    section 23: part 207: rest controller

---
+++ src/main/java/com/cercetare/ecommerce/controller/CheckoutController.java
@@ -0,0 +1,25 @@
+package com.cercetare.ecommerce.controller;
+
+import com.cercetare.ecommerce.dtos.Purchase;
+import com.cercetare.ecommerce.dtos.PurchaseResponse;
+import com.cercetare.ecommerce.service.CheckoutService;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
+}
