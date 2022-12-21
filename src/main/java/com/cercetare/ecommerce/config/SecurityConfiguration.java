b4aa30cae8b18850051340170a3f242a61b31bca
Iancu Jianu <iancujianu98@gmail.com>
2022-12-21 20:35:41 +0200

    section 30: part 262 -> 266: secure order history rest api Steps: 1. add okta spring boot starter to maven pom.xml 2. in springboot app, set application properties 3. protect endpoints in spring security configuration class

---
+++ src/main/java/com/cercetare/ecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package com.cercetare.ecommerce.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        // protect endpoint /api/orders
+        httpSecurity
+                .authorizeHttpRequests(
+                        configurer ->
+                                configurer
+                                .antMatchers("/api/orders/**")
+                                .authenticated())
+                .oauth2ResourceServer()
+                .jwt();
+
+        // add CORS filter
+        httpSecurity.cors();
+
+        // add content negotiation strategy
+        httpSecurity.setSharedObject(ContentNegotiationStrategy.class, new HeaderContentNegotiationStrategy());
+
+        // force a non-empty response body for 401's to make the response more friendly
+        Okta.configureResourceServer401ResponseBody(httpSecurity);
+
+        return httpSecurity.build();
+    }
+}
