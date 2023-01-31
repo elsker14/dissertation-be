0d1a6330bb04713e7a4f7a34e8065d220f7c97bd
Iancu Jianu <iancujianu98@gmail.com>
2023-01-31 10:02:17 +0200

    added unit tests for ProductRepository

--- src/main/java/com/cercetare/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/cercetare/ecommerce/dao/ProductRepository.java
@@ -7,15 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.RequestParam;
 
-//@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 
     Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 }
-
-/*
-Info:
-    Adaugam adnotarea cu CrossOrigin in step60 pentru a putea permite lui SpringBoot sa accepte scripturi web cu de la surse cu origine diferita
- */
---
+++ src/test/java/com/cercetare/ecommerce/unit/ProductRepositoryTests.java
@@ -0,0 +1,53 @@
+package com.cercetare.ecommerce.unit;
+
+import com.cercetare.ecommerce.dao.ProductRepository;
+import com.cercetare.ecommerce.entity.Product;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+public class ProductRepositoryTests {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void itShouldSelectAllProducts() {
+        List<Product> allProducts = productRepository.findAll();
+
+        assertThat(allProducts).isNotNull();
+        allProducts.forEach(it -> assertThat(it.getName()).isNotNull());
+    }
+
+    @ParameterizedTest
+    @ValueSource(longs = {1,2,3,4})
+    void itShouldSelectOnlyProductsFromEachCategory(Long parameterCategoryId) {
+        Page<Product> allProductsFromCategory = productRepository.findByCategoryId(parameterCategoryId, Pageable.ofSize(100));
+
+        allProductsFromCategory.forEach(it -> assertThat(it.getCategory().getId()).isEqualTo(parameterCategoryId));
+    }
+
+    @RepeatedTest(10)
+    void itShouldReturnExistingProductsStartingWithName() {
+        List<String> productNames = new ArrayList<>(Arrays.asList("python", "java", "sql", "coffee", "pad"));
+        Random random = new Random();
+        int randomIndex = random.nextInt(productNames.size());
+
+        System.out.println("This test checks all returned products contain: " + productNames.get(randomIndex));
+        Page<Product> foundProductsStartingWithName = productRepository.findByNameContaining(productNames.get(randomIndex), Pageable.ofSize(100));
+        foundProductsStartingWithName.forEach(it -> assertThat(it.getName().toLowerCase().contains(productNames.get(randomIndex))).isTrue());
+    }
+}
